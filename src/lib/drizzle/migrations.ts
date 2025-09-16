e9f86efffa07d57442ff303c4f2980c729971f31
tigawanna <denniskinuthiaw@gmail.com>
2025-09-17 00:56:49 +0300

    flatist

--- src/lib/drizzle/migrations.ts
+++ src/lib/drizzle/migrations.ts
@@ -1,45 +1,25 @@
-// import ExpoSpatialiteModule from "@/modules/expo-spatialite/src/ExpoSpatialiteModule";
-
-const keyna_wards = `
-CREATE TABLE kenya_wards IF NOT EXISTS (
-	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	ward_code text,
-	ward text NOT NULL,
-	county text NOT NULL,
-	county_code integer,
-	sub_county text,
-	constituency text NOT NULL,
-	constituency_code integer,
-);
-
-SELECT AddGeometryColumn('kenya_wards', 'geom', 4326, 'MULTIPOLYGON', 'XY');
-`
 const notes = `
 CREATE TABLE notes (
 	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	title text,
 	content text NOT NULL,
+	quick_copy text,
 	type text DEFAULT 'note' NOT NULL,
-	value real,
+	status text DEFAULT 'active',
+	tags text,
+	metadata text,
+	image_path text,
+	image_blob blob,
+	priority integer DEFAULT 0,
+	reminder_at text,
+	completed_at text,
+	due_date text,
+	last_viewed text DEFAULT (CURRENT_TIMESTAMP),
 	created_at text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
 	updated_at text DEFAULT (CURRENT_TIMESTAMP) NOT NULL
-	-- location GEOMETRY
 );
 
-SELECT AddGeometryColumn('notes', 'location', 4326, 'POINT', 'XY');
-`
+SELECT AddGeometryColumn('notes', 'location_point', 4326, 'POINT', 'XY');
+`;
 
-export const migrations = [
-    keyna_wards,
-    notes
-]
-
-
-// function runMigrations(){
-//     ExpoSpatialiteModule.executeTransaction(
-//         [
-//             {sql}
-//         ]
-//     )
-// }
-
+export const migrations = [notes];
