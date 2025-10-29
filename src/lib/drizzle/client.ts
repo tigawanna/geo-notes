06f56f710fcf85ab65cef3bf0fc0a4d9d20ca5cd
tigawanna <denniskinuthiaw@gmail.com>
2025-10-30 01:46:37 +0300

    starting point

--- src/lib/drizzle/client.ts
+++ src/lib/drizzle/client.ts
@@ -6,7 +6,8 @@ const opsqliteDb = open({
   name: "myDB",
 });
 const path = "libspatialite";
-opsqliteDb.loadExtension(path, "sqlite3_modspatialite_init");
+const entryPoint = "sqlite3_modspatialite_init";
+opsqliteDb.loadExtension(path, entryPoint);
 export const db = drizzle(opsqliteDb, {
   logger: true,
   schema: schema,
