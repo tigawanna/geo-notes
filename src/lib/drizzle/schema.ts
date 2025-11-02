d2afd8e9bfec4c5a8d69372843c7e66025ddd0d4
tigawanna <denniskinuthiaw@gmail.com>
2025-11-02 21:20:10 +0300

    Refactor notes schema and migration files; update database initialization and environment variables

--- src/lib/drizzle/schema.ts
+++ src/lib/drizzle/schema.ts
@@ -2,28 +2,24 @@ import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { sql } from "drizzle-orm/sql";
 import * as t from "drizzle-orm/sqlite-core";
 import { sqliteTable as table } from "drizzle-orm/sqlite-core";
-import * as Crypto from "expo-crypto";
 import { point } from "./drizzlespatialite-types";
 import { createInsertSchema } from "drizzle-zod";
 
 export const notes = table(
   "notes",
   {
-    id: t.text().primaryKey().default(Crypto.randomUUID()),
+    id: t.text().primaryKey().notNull(),
     title: t.text("title").default("Untitled"),
-    quickCopy: t.text("quick_copy"),// field that will be copied on long click
+    quickCopy: t.text("quick_copy"), // field that will be copied on long click
     content: t.text("content"),
     created: t.text().default(sql`(CURRENT_TIMESTAMP)`),
     updated: t.text().default(sql`(CURRENT_TIMESTAMP)`),
     location: point("location"), // ← this is correct for WKB geometry
     user_id: t.text("user_id"),
   },
-  (table) => [
-    t.index("idx_notes_id").on(table.id),
-    t.index("idx_notes_created").on(table.created),
-  ]
+  (table) => [t.index("idx_notes_id").on(table.id), t.index("idx_notes_created").on(table.created)]
 );
 
 export type TNote = InferSelectModel<typeof notes>;
 export type TInsertNote = InferInsertModel<typeof notes>;
-export const InsertNoteZodSchema = createInsertSchema(notes)
+export const InsertNoteZodSchema = createInsertSchema(notes);
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,10 +7,10 @@ import {z} from "zod";
 // };
 
 export const envVariablesSchema = z.object({
-  EXPO_PUBLIC_SUPABASE_URL: z.url(),
-  EXPO_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-  EXPO_PUBLIC_SUPABASE_BUCKET: z.string().default(""),
-  EXPO_PUBLIC_POWERSYNC_URL: z.url(),
+  // EXPO_PUBLIC_SUPABASE_URL: z.url(),
+  // EXPO_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  // EXPO_PUBLIC_SUPABASE_BUCKET: z.string().default(""),
+  // EXPO_PUBLIC_POWERSYNC_URL: z.url(),
 });
 
 export const envVariables = envVariablesSchema.parse(process.env);
