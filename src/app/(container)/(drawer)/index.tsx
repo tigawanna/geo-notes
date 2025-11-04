20ce1716e1036309abeeb5f7bc97f863c0d04cd6
tigawanna <denniskinuthiaw@gmail.com>
2025-11-04 15:22:32 +0300

    clean up assets

--- src/app/(container)/(drawer)/index.tsx
+++ src/app/(container)/(drawer)/index.tsx
@@ -5,7 +5,14 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 export default function HomeScreen() {
   const { top, bottom } = useSafeAreaInsets();
   return (
-    <Surface style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingTop: top + 10, paddingBottom: bottom }}>
+    <Surface
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: top + 10,
+        paddingBottom: bottom,
+      }}>
       <Notes />
     </Surface>
   );
