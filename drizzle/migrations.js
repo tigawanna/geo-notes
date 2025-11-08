4fa2f31606b4ab6a295c50fe5600a0e0a8086bc2
tigawanna <denniskinuthiaw@gmail.com>
2025-11-08 04:14:58 +0300

    feat: Refactor database schema and migration files for notes and tags, removing obsolete files and updating journal references

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,12 +1,12 @@
+// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
+
 import journal from './meta/_journal.json';
-import m0000 from './0000_productive_tusk.sql';
-import m0001 from './0001_load-patial_ref_sys.sql';
+import m0000 from './0000_curved_thaddeus_ross.sql';
 
   export default {
     journal,
     migrations: {
-      m0000,
-m0001
+      m0000
     }
   }
   
